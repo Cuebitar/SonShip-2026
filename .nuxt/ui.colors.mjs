6e2ce7235369ffb82329b3087ae69551b5dbc2b9
Shawn Liu Han Sheng <Shawnlhs01@gmail.com>
2026-03-25 23:49:14 +0800

    change background and unuse file

--- .nuxt/ui.colors.mjs
+++
@@ -1,1 +0,0 @@
-export default ["red","orange","amber","yellow","lime","green","emerald","teal","cyan","sky","blue","indigo","violet","purple","fuchsia","pink","rose","primary","secondary","highlight","accent","card","popover","muted","destructive","chart"];
